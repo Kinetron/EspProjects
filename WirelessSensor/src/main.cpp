bda212769b4531f631ab7c692ef2cd3341a7d02b
John <TPgMNMKQLre897@rs.ru>
2025-02-03 20:26:32 +0300

    Begin work a sensor project

---
+++ WirelessSensor/src/main.cpp
@@ -0,0 +1,12 @@
+#include <Arduino.h>
+
+void setup() {
+  pinMode(LED_BUILTIN, OUTPUT);
+}
+
+void loop() {
+  delay(300);
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(300);
+  digitalWrite(LED_BUILTIN, LOW); 
+}
